03eaa832f6e76b49607db501f5692bbb1302bb90
Sighvatur Sveinn Davidsson <sdavidsson90@gmail.com>
2024-02-25 23:09:09 +0100

    Paste multiple files

--- README.md
+++ README.md
@@ -5,14 +5,16 @@ These functions will allow you to copy something and drop it off somewehere else
 
 The idea is that you should not have to spell out the full path to a file in a single command, but navigate to the file, do the copy action, navigate to a desired location and do the paste action.
 
-#### What functions are there?
-- `cpwd`: copy working directory
-- `cpfc`: copy file contents
+#### How would I do that?
 - `cpfp`: copy file path
-- `p`: paste clipboard content to stdout
-- `pp`: execute clipboard content as a command
 - `pf`: paste file to current working directory
 - `mvf`: move file to current working directory
+
+#### Additional functions
+- `cpwd`: copy working directory
+- `cpfc`: copy file contents
+- `p`: paste clipboard content to stdout
+- `px`: execute clipboard content as a command
 
 #### How do I use them? 
 Assuming that $LOCATION is the location of the script on your system, add this line to your `.bashrc`/`.zshrc`:
--- copytools.sh
+++ copytools.sh
@@ -1,10 +1,18 @@
 # Identify clipboard tool
 if command -v pbcopy &> /dev/null; then
-  copy() { pbcopy }
-  paste() { pbpaste }
+  copy() { 
+    pbcopy
+  }
+  paste() { 
+    pbpaste
+  }
 elif command -v xsel &> /dev/null; then
-  copy() { xsel -ib }
-  paste() { xsel -ob }
+  copy() { 
+    xsel -ib
+  }
+  paste() { 
+    xsel -ob
+  }
 else
   echo "It seems you don't have a supported clipboard tool!"
   return 1
@@ -12,32 +20,36 @@ fi
 
 # Copy working directory
 cpwd() {
-  echo -n $(dirs) | copy && \
-  echo -e "\e[1mCopied working directory!\e[0m"
+  echo -n "$(dirs)/" | copy && \
+  echo -e "\e[1mCopied working directory:\e[0m"
+  paste
 }
 
 # Copy file contents
 cpfc() {
-  filename=$(basename $1)
-  cat $1 | copy && \
-  echo -e "\e[1mCopied content of:\e[0m $filename"
+  cat "$@" | copy && \
+  echo -en "\e[1mCopied content of: \e[0m"
+  echo $@
 }
-
 
 # Copy filepath
 cpfp() {
-  filepath=$(realpath $1)
-  echo $filepath | copy
-  echo -e "\e[1mCopied the following to clipboard:\e[0m\n$filepath"
+  FILEPATHS=""
+  echo -e "\e[1mCopied filepaths:\e[0m"
+  for i in "$@"; do
+    file=$(realpath "$i")
+    FILEPATHS+="$file"
+    FILEPATHS+=" "
+    echo $file| sed 's/ /\n/'
+  done
+  printf '%s' "${FILEPATHS[@]}"|copy
 }
-
 
 # Paste clipboard content to stdout
 p() { paste }
 
-
-# Execute clipboard content as command
-pp() {
+# Paste and execute as command
+px() {
   echo -e "\e[1mClipboard content:\e[0m\n$(paste)"
   echo -en "\e[1mDo you want to execute this as a command? [Y/n]:\e[0m "
   local response=$(bash -c "read -n 1 response; echo \$response")
@@ -66,32 +78,35 @@ pp() {
 
 # Paste file to current location
 pf() {
-  FILENAME=$(basename $(paste))
-  if [ -e $FILENAME ]; then
-    echo -en "\e[1m$FILENAME already exists! Do you wish to overwrite? [Y/n]:\e[0m "
-    local response=$(bash -c "read -n 1 response; echo \$response")
-  fi
-  if [ ! -z $response ]; then
-    local RESPONSE=$response
-  else
-    local RESPONSE='Y'
-  fi
-  case $RESPONSE in
-    [Yy]) 
-        FILE=$(paste)
+  FILEPATHS="$(paste)"
+  echo -e "\e[1mPasting files:\e[0m"
+  for FILE in $(echo $FILEPATHS); do
+    FILENAME=$(basename $FILE)
+    echo -n $FILE
+    if [ -e $FILENAME ]; then
+      echo -e "\nThis file already exists! Do you wish to overwrite? [Y/n]:"
+      local response=$(bash -c "read -n 1 response; echo \$response")
+    fi
+    if [ ! -z $response ]; then
+      local RESPONSE=$response
+    else
+      local RESPONSE='Y'
+    fi
+    case $RESPONSE in
+      [Yy])
         cp -r $FILE . 2>/dev/null && \
-        echo -e "\nSuccesfully pasted $FILENAME to current directory!" || \
-        echo -e "\nSomething went wrong:" && \
-        cp -r $FILE .
-      ;;
-    [Nn])
-      echo -e "\nNo action taken!"
-      ;;
-    *)
-      echo -e "\nInvalid response!"
-      return 1
-      ;;
-  esac
+          echo " ✓" || \
+          echo " ✘"
+        ;;
+      [Nn])
+        echo " ✘"
+        ;;
+      *)
+        echo -e "\nInvalid response!"
+        return 1
+        ;;
+    esac
+  done
 }
 
 
@@ -123,3 +138,4 @@ mvf() {
       ;;
   esac
 }
+
